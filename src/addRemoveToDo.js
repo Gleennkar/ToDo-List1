cdca7787733a74c174a9528b0102027e44b18e7a
Odette Gleen <nkargleeny@gmail.com>
2022-08-19 16:08:26 +0100

    checking for errors

--- src/addRemoveToDo.js
+++ src/addRemoveToDo.js
@@ -3,21 +3,19 @@ if (storedList === null) {
   storedList = [];
 }
 
-
-
 export const add = (description) => {
-    const todolist = [...storedList];
-    todolist.push({
-      index: Date.now(),
-      description,
-      completed: false,
-    });
-    localStorage.setItem('tasks', JSON.stringify(todolist));
-  };
-  
-  export const removeTodo = (id) => {
-    const todolist = storedList.filter((todo) => todo.index.toString() !== id);
-    localStorage.setItem('tasks', JSON.stringify(todolist));
-    getTodoList(storedList);
-    window.location.reload();
-  };
+  const todolist = [...storedList];
+  todolist.push({
+    index: Date.now(),
+    description,
+    completed: false,
+  });
+  localStorage.setItem('tasks', JSON.stringify(todolist));
+};
+
+export const removeTodo = (id) => {
+  const todolist = storedList.filter((todo) => todo.index.toString() !== id);
+  localStorage.setItem('tasks', JSON.stringify(todolist));
+  getTodoList(storedList);
+  window.location.reload();
+};
--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
 import './style.css';
-
-const todolist = [
-
-];
-
-let storedList = JSON.parse(localStorage.getItem('tasks'));
-if (storedList === null) {
-  storedList = [];
-}
-
-const add = (description) => {
-  todolist.push({
-    index: todolist.length + 1,
-    description,
-    completed: false,
-  });
-  localStorage.setItem('tasks', JSON.stringify(todolist));
-};
-
-const input = document.querySelector('.todo-input');
-input.addEventListener('keyup', (e) => {
-  if (e.keyCode === 13) {
-    add(input.value);
-    const storedList = JSON.parse(localStorage.getItem('tasks'));
-    // eslint-disable-next-line no-unused-vars
-    getTodoList(storedList);
-    input.value = ' ';
-  }
-  window.location.reload();
-});
+import { add, removeTodo, storedList } from './addRemoveToDo.js';
+// let todolist = [];
 
 class Tasks {
   constructor() {
@@ -45,15 +17,23 @@ class Tasks {
 
   remove(id) {
     this.toDoList = this.toDoList.filter((list) => list.id !== Number(id));
+    // function remove(element)
+    {
+      const index = element.parentNode.id;
+      toDoList.splice(index, 1);
+      return toDoList;
+    }
   }
 }
 // eslint-disable-next-line no-unused-vars
 const list1 = new Tasks();
 const todoDiv = document.querySelector('.list1');
+
 const getTodoList = (array) => {
   array.forEach((list) => {
     const li = document.createElement('li');
     li.classList.add('list');
+    li.id = list.index;
     const liDiv = document.createElement('div');
     liDiv.classList.add('li-div');
 
@@ -62,16 +42,55 @@ const getTodoList = (array) => {
     checkbox.checked = list.completed;
     liDiv.appendChild(checkbox);
 
-    const desc = document.createElement('p');
-    desc.innerText = list.description;
+    const desc = document.createElement('input');
+    desc.classList.add('desc');
+    desc.value = list.description;
+    desc.onblur = (e) => {
+      saveone(desc.value, e.target.parentNode.parentNode.id);
+    };
     liDiv.appendChild(desc);
     li.appendChild(liDiv);
+
+    const trash = document.createElement('i');
+    trash.classList.add('fa');
+    trash.classList.add('fa-trash-o');
+    trash.addEventListener('click', (e) => {
+      removeTodo(e.target.parentNode.id);
+    });
+    trash.classList.add('hide');
+    li.appendChild(trash);
 
     const dots = document.createElement('i');
     dots.classList.add('fa');
     dots.classList.add('fa-ellipsis-v');
+    dots.classList.add('dot');
+    dots.addEventListener('click', () => {
+      dots.classList.add('hide');
+      trash.classList.remove('hide');
+    });
     li.appendChild(dots);
+
     todoDiv.appendChild(li);
   });
 };
+
+const saveone = (desc, id) => {
+  const myTodo = storedList.find((todo) => todo.index.toString() == id);
+  myTodo.description = desc;
+  const todoPosition = storedList.indexOf(myTodo);
+  storedList[todoPosition] = myTodo;
+  localStorage.setItem('tasks', JSON.stringify(storedList));
+};
+
+const input = document.querySelector('.todo-input');
+input.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    add(input.value);
+    const storedList = JSON.parse(localStorage.getItem('tasks'));
+    getTodoList(storedList);
+    input.value = ' ';
+    window.location.reload();
+  }
+});
+
 getTodoList(storedList);
